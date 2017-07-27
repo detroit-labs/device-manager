1807da084597be7f4b2c9bc46b2817aae68737c9
Peike Dai <peike.dai@detroitlabs.com>
2017-07-26 23:24:34 -0400

    Add authentication
    The DB has sensitive date so it should not be public to the world
    Add anonymous authentication method

--- app/src/main/java/com/detroitlabs/devicemanager/SyncActivity.java
+++ app/src/main/java/com/detroitlabs/devicemanager/SyncActivity.java
@@ -6,19 +6,26 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.annotation.StringRes;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 
 import com.detroitlabs.devicemanager.databinding.ActivitySyncBinding;
 import com.detroitlabs.devicemanager.sync.RegistrationService;
 import com.detroitlabs.devicemanager.sync.SyncingService;
 import com.detroitlabs.devicemanager.utils.DeviceUtil;
 import com.detroitlabs.devicemanager.utils.PrefUtil;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import static com.detroitlabs.devicemanager.constants.Constants.BROADCAST_ACTION_REGISTER_RESULT;
 import static com.detroitlabs.devicemanager.constants.Constants.BROADCAST_ACTION_SYNC_RESULT;
@@ -37,13 +44,42 @@ public class SyncActivity extends AppCompatActivity {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_sync);
 
         initReceiver();
-        checkRegisterStateAndInitSyncing();
+        authenticate();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         LocalBroadcastManager.getInstance(this).unregisterReceiver(receiver);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSION_REQUEST_GET_ACCOUNTS) {
+            if (grantResults.length > 0 &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                registerDevice();
+                return;
+            }
+            syncDatabase();
+        }
+    }
+
+    private void authenticate() {
+        updateStatusText(R.string.authentication);
+        FirebaseAuth.getInstance().signInAnonymously().addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    checkRegisterStateAndInitSyncing();
+                } else {
+                    Log.w(TAG, "linkWithCredential:failure", task.getException());
+//                    startPagerActivity();
+                    updateStatusText(R.string.not_authorized);
+                    binding.progressBar.setVisibility(View.GONE);
+                }
+            }
+        });
     }
 
     private void initReceiver() {
